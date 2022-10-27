52cdc81ae0126326f1b083079c63b3fa81ef0b17
«Ilya <333zero777@mail.ru>
2022-10-27 20:54:05 +0300

    добавлен лог

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
 ﻿
 using MiniCalc;
+using static System.Net.Mime.MediaTypeNames;
+using System.IO;
+
 int value1 = 0;
 int value2 = 0;
 Console.WriteLine("Введите 2 числа");
@@ -11,4 +14,8 @@ try
 catch (Exception ex) { Console.WriteLine("Введено не число"); }
 Calc calc = new Calc();
 int result = calc.Summer(value1, value2);
+using (StreamWriter writer = new StreamWriter("C:\\Users\\Admin\\source\\repos\\MiniCalc\\log.txt", false))
+{
+    await writer.WriteLineAsync("Суммировалось " + value1 + " и " + value2 + " Результат " + result);
+}
 Console.WriteLine(result);
